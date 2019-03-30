e294c30c71e801d7f3e7ce052227264f433885d9
Lê Trọng Anh Đức <ducltauit@gmai.com>
2019-03-30 18:35:04 +0700

    update tour_detail finish img_demo

--- js/tour_detail.js
+++ js/tour_detail.js
@@ -68,27 +68,64 @@ $(document).ready(function () {
 $(document).ready(function () {
     var imgs = $(".img-slideshow img");
     var slideIndex = 0;
+    var imgThumbnail = $(".img-info .item")
     $(".btn-next-img").click(function () {
         imgs.eq(slideIndex).fadeOut();
         imgs.eq(slideIndex).addClass("d-none");
+        imgThumbnail.eq(slideIndex).removeClass("active");
+        s1 = imgThumbnail.eq(slideIndex).offset().top;
         if (slideIndex == imgs.length - 1)
             slideIndex = 0;
         else
             slideIndex++;
         imgs.eq(slideIndex).fadeIn();
         imgs.eq(slideIndex).removeClass("d-none");
+        var item = imgThumbnail.eq(slideIndex);
+        var imgInfo = $(".img-info");
+        item.addClass("active");
+        var s2 = item.offset().top;
+        var s = Math.abs(s1 - s2);
+        if (item.offset().top + item.height() > imgInfo.offset().top + imgInfo.height())
+            $(".img-info").animate({ scrollTop: '+=' + s }, 300, 'swing');
+        if(slideIndex == 0)
+            $(".img-info").animate({ scrollTop: '-=' + s }, 300, 'swing');
     });
     $(".btn-pre-img").click(function () {
         imgs.eq(slideIndex).fadeOut();
         imgs.eq(slideIndex).addClass("d-none");
+        imgThumbnail.eq(slideIndex).removeClass("active")
+        s1 = imgThumbnail.eq(slideIndex).offset().top;
         if (slideIndex == 0)
             slideIndex = imgs.length - 1;
         else
             slideIndex--;
         imgs.eq(slideIndex).fadeIn();
         imgs.eq(slideIndex).removeClass("d-none");
+        var item = imgThumbnail.eq(slideIndex);
+        var imgInfo = $(".img-info");
+        item.addClass("active");
+        var s2 = item.offset().top;
+        var s = Math.abs(s1 - s2);
+        if (item.offset().top < imgInfo.offset().top)
+            $(".img-info").animate({ scrollTop: '-=' + s }, 300, 'swing');
+        if (slideIndex == imgs.length - 1)
+            $(".img-info").animate({ scrollTop: '+=' + s }, 300, 'swing');
+    });
+
+    imgThumbnail.each(function (index) {
+        $(this).click(function (e) {
+            if (!$(this).hasClass("active")) {
+                imgs.eq(slideIndex).fadeOut();
+                imgs.eq(slideIndex).addClass("d-none");
+                imgThumbnail.eq(slideIndex).removeClass("active");
+
+                slideIndex = index;
+                imgs.eq(slideIndex).fadeIn();
+                imgs.eq(slideIndex).removeClass("d-none");
+                $(this).addClass("active");
+            }
+        });
     });
 });
-
 
 
